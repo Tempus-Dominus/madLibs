1cb3f383429ecec13e74ef079960fbeefed9ec4a
Tempus-Dominus <bioerror@hotmail.com>
2022-02-02 13:32:24 -0700

    Imported code from Screens

--- App.js
+++ App.js
@@ -1,13 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    Component={HomeScreen}
+                    options={{ title: 'Home' }}
+                />
+
+                <Stack.Screen
+                    name="Profile"
+                    Component={ProfileScreen}
+                    options={{ title: 'Profile' }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+        )
+}
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            usernameText: "",
+        }
+    }
+
+    HomeScreen = ({ navigation, route }) => {
+        let name;
+
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.textInput}
+                    value={name}
+                    placeholder={"User Name"}
+                    onChangeText={newName => { name = newName; this.setState({ usernameText: name }) }}
+                />
+                <Button
+                    title="Go to Profile"
+                    onPress={() => { navigation.navigate('Profile', { profileName: name }) }}
+                    />
+
+                <StatusBar style="auto" />
+            </View>
+        )
+    }
+
+    ProfileScreen = ({ navigation, route }) => {
+
+        return (
+            <View style={styles.container}>
+                <Text>Welcome to the profile screen</Text>
+                <Text> We received {route.params.profileName} from Home.</Text>
+                <Text> This is the name from the state variable: {this.state.usernameText}</Text>
+                <StatusBar style="auto" />
+            </View>
+        )
+    }
+
+    render() {
+        
+        return (
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Home"
+                        component={this.HomeScreen}
+                        options={{ title: 'Home' }}
+                    />
+
+                    <Stack.Screen
+                        name="Profile"
+                        component={this.ProfileScreen}
+                        options={{ title: 'Profile' }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +95,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    },
+  textInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
